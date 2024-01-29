ca9ca96ba19fba4a707852858e47b22734e2e284
Guilherme Marzano <guilhermemarzano@gmail.com>
2024-01-29 18:42:43 -0300

    Changed test logic file

--- test_logic.py
+++ test_logic.py
@@ -1,6 +1,12 @@
-from app_classes import Livros
+from app_classes import Livros, Membros
 
 
 l1 = Livros('l001','Senhor dos aneis','J.J.Token')
-
+l2 = Livros('l002','Robbits','j.j.token')
 print(l1)
+
+m1_livros =[l1.titulo,l2.titulo]
+
+m1 = Membros('m001','gui',m1_livros)
+
+print(m1)
